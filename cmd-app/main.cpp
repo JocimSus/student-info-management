cd23d20bff0074323a308d1564b326d865000095
Joachim Susatiyo <joe.susatiyo@gmail.com>
2024-03-14 16:40:13 +0700

    Switch to enums for clairty

--- cmd-app/main.cpp
+++ cmd-app/main.cpp
@@ -4,7 +4,14 @@
 
 #include "./adminFunctions.h"
 #include "./passwordEncryption.h"
-#include "./utilities.h"
+
+enum LoginChoice { Admin = 1,
+                   Student = 2 };
+enum AdminChoice { Add_Student_Details = 1,
+                   Delete_Students = 2,
+                   Admin_View_Table = 3,
+                   Main_Menu = 4 };
+enum StudentChoice { Student_View_Table = 1 };
 
 int main() {
     while (true) {
@@ -14,17 +21,16 @@ int main() {
                   << "2. Student" << std::endl
                   << "3. Exit" << std::endl
                   << std::endl
-                  << "Enter Your Choice :" << std::endl
+                  << "Enter Your Choice (1-3):" << std::endl
                   << "> ";
 
-        std::string login_choice = "";
+        int login_choice;
         std::cin >> login_choice;
-        login_choice = lowerString(login_choice);
 
         std::cout << std::endl;
 
-        std::string admin_password = "";
-        if ((login_choice == "1") || (login_choice == "admin")) {
+        if (login_choice == Admin) {
+            std::string admin_password = "";
             while (true) {
                 if (std::filesystem::exists("password.txt")) {
                     // Login
@@ -33,9 +39,11 @@ int main() {
                               << "Enter Password :" << std::endl
                               << "> ";
                     std::cin >> admin_password;
-                    if ((admin_password != "") && (authenticatePassword(admin_password))) {
+
+                    if (!(admin_password.empty()) && (authenticatePassword(admin_password))) {
                         std::cout << "Login Successful" << std::endl
                                   << std::endl;
+                        admin_password.clear();  // clears password from stack
                         break;
                     } else {
                         std::cout << "Wrong Password" << std::endl
@@ -48,13 +56,13 @@ int main() {
                               << "Enter Password :" << std::endl
                               << "> ";
                     std::cin >> admin_password;
-                    if (admin_password != "") {
+
+                    if (!(admin_password.empty())) {
                         savePassword(admin_password);
                     }
                 }
             }
             while (true) {
-                char admin_choice = 0;
                 std::cout << "| Logged in as Admin |" << std::endl
                           << std::endl
                           << "1. Add Students Details" << std::endl
@@ -63,56 +71,59 @@ int main() {
                           << "4. Main Menu" << std::endl
                           << "5. Exit" << std::endl
                           << std::endl
-                          << "Enter Your Choice :" << std::endl
+                          << "Enter Your Choice (1-5):" << std::endl
                           << "> ";
+
+                int admin_choice = 0;
                 std::cin >> admin_choice;
                 switch (admin_choice) {
-                    case '1':
+                    case Add_Student_Details:
                         std::cout << "1";
+                        // func
                         break;
-                    case '2':
+                    case Delete_Students:
                         std::cout << "2";
+                        // func
                         break;
-                    case '3':
+                    case Admin_View_Table:
                         std::cout << "3";
+                        // func
                         break;
-                    case '4':
+                    case Main_Menu:
                         std::cout << "4";
-
+                        break;
                     default:
                         std::cout << "Exiting...";
-                        break;
+                        return 0;
                 }
                 break;
             }
-        } else if ((login_choice == "2") || (login_choice == "student")) {
+        } else if (login_choice == Student) {
             while (true) {
-                char student_choice = 0;
                 std::cout << "| Logged in as Student |" << std::endl
                           << std::endl
                           << "1. View Table" << std::endl
                           << "2. Exit" << std::endl
                           << std::endl
-                          << "Enter Your Choice :" << std::endl
+                          << "Enter Your Choice (1-2):" << std::endl
                           << "> ";
+                int student_choice = 0;
                 std::cin >> student_choice;
                 switch (student_choice) {
-                    case '1':
+                    case Student_View_Table:
                         std::cout << "1";
+                        // func
                         break;
-
                     default:
                         std::cout << "Exiting...";
-                        break;
+                        return 0;
                 }
                 break;
             }
-            break;
         } else {
             std::cout << "Exiting...";
             break;
         }
     }
-
     return 0;
 }
